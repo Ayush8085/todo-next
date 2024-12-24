fa1c4e8a750017439ce830817adc0535611f5521
Ayush <ayushkumar9601@gmail.com>
2024-12-24 16:18:05 +0530

    navbar with google login done

---
+++ components/Navbar.tsx
@@ -0,0 +1,45 @@
+import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
+import { Button } from "./ui/button"
+import { auth, signIn, signOut } from "@/auth";
+
+const Navbar = async () => {
+    const session = await auth();
+
+    return (
+        <header className="w-full border-2 p-2 flex justify-between">
+            <div className="">
+                <h1 className="text-2xl font-24-medium">Todo App</h1>
+            </div>
+
+            <div className="flex gap-2">
+                {session?.user ? (
+                    <>
+                        <form
+                            action={async () => {
+                                "use server"
+                                await signOut()
+                            }}
+                        >
+                            <Button type="submit">SignOut</Button>
+                        </form>
+                        <Avatar>
+                            <AvatarImage src={session.user.image!} />
+                            <AvatarFallback>{session.user.name}</AvatarFallback>
+                        </Avatar>
+                    </>
+                ) : (
+                    <form
+                        action={async () => {
+                            "use server"
+                            await signIn("google")
+                        }}
+                    >
+                        <Button type="submit">SignIn with Google</Button>
+                    </form>
+                )}
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
